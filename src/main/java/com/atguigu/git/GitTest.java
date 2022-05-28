0ce7254d315b116aae4d7148047e1e8f7b8242ba
hx <hx@atguigu.com>
2022-05-28 13:34:18 +0800

    OEM test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("master tset");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("OEM test");
 
     }
 
